7568aaa811e2beea5bddd3280561720b36b2b492
Rob Abby <robabby23@gmail.com>
2014-09-17 16:40:59 -0500

    Getting caught up...

---
+++ app/scripts/nav.js
@@ -0,0 +1,28 @@
+(function(w, $) {
+	var $navToggle = $('.nav-toggle'),
+		$drawer = $('#drawer'),
+		$pageWrap = $('#page-wrap'),
+		$header = $('.header')
+		drawerOpen = false;
+
+	$navToggle.on('click', animatePage);
+
+	function animatePage() {
+		if(!drawerOpen) {
+			drawerOpen = true;	
+		
+			$navToggle.addClass('open');
+			$drawer.addClass('open');
+			$header.addClass('open');
+			$pageWrap.addClass('open');
+
+		} else {
+			drawerOpen = false;
+		
+			$navToggle.removeClass('open');
+			$drawer.removeClass('open');
+			$header.removeClass('open');
+			$pageWrap.removeClass('open');
+		}
+	}
+})(window, window.jQuery);
